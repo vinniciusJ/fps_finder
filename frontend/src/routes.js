03cce76320712c3537e7b9e53d1c5580a570338b
Vinicius <vinnicius.jimenez@gmail.com>
2020-12-18 10:06:03 -0300

    Set up project

---
+++ frontend/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './pages/Home/Home'
+
+const Routes = () => (
+    <BrowserRouter>
+        <Route path='/' exact component={Home}/>
+    </BrowserRouter>
+)
+
+export default Routes
