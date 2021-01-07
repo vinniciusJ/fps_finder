23a7d03f316f3b56a27e1a18d692c759e454cb50
Vinicius <vinnicius.jimenez@gmail.com>
2021-01-07 16:36:57 -0300

    Combination has been created

---
+++ frontend/src/pages/Combination/Combination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './styles.css'
+
+const Combination = () => {
+    return (
+        <div><h1>Hello, World!</h1></div>
+    )
+}
+
+export default Combination
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,13 +4,14 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import AdminPanel from './pages/AdminPanel/AdminPanel'
+import Combination from './pages/Combination/Combination'
 
 const Routes = () => (
     <BrowserRouter>
         <Route path='/' exact component={Home} />
         <Route path='/login' component={Login} />
         <Route path='/admin' component={AdminPanel} />
-        <Route path='/combination' component={() => <h1>Hello</h1>}/>
+        <Route path='/combination' component={Combination}/>
     </BrowserRouter>
 )
 
