188fff14696bc06ec29dabae84008f6aa23edff9
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-03-30 12:26:59 -0300

    Finalizando heroku

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import environ
 import os
+import django_heroku
 
 env = environ.Env()
 environ.Env.read_env()
@@ -24,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://fpsfinder-blog.herokuapp.com/']
 
 
 # Application definition
@@ -146,3 +147,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
